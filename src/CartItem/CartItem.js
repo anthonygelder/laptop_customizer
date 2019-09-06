c9c35c9dfa90994a4fa276483e9ef125eadd23e7
Anthony Gelder <anthony.gelder@gmail.com>
2019-09-05 21:27:30 -0700

    commit

---
+++ src/CartItem/CartItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Customize.css';
+
+
+class CartItem extends Component {
+  render() {
+    return (
+        <div className="summary__option" key={featureHash}>
+          <div className="summary__option__label">{feature} </div>
+          <div className="summary__option__value">{selectedOption.name}</div>
+          <div className="summary__option__cost">
+            {USCurrencyFormat.format(selectedOption.cost)}
+          </div>
+        </div>
+    );
+  }
+}
+
+export default CartItem;
